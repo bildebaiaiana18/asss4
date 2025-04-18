162054db2525e1997d23d2492cf6017cec5e216f
bildebaiaiana18 <bildebai.aiana@narxoz.kz>
2025-04-18 19:12:54 +0500

    акщте ф
    https://github.com/Akmaral06/HealthTrack.git

---
+++ UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.healthtrack.repository;
+
+import com.example.healthtrack.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
